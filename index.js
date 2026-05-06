f705d07ba08df4d02507a541b126b509b7918031
Tarcisio Henrique <134089581+tarcisiohenriqueassis@users.noreply.github.com>
2026-05-06 08:35:37 -0300

    Refactor form field handling and enable flattening
    Removed field appearance updates and enabled flattening of form fields.

--- index.js
+++ index.js
@@ -64,10 +64,7 @@ app.post("/gerar-pdf-template", async (req, res) => {
   
 });
 
-form.updateFieldAppearances();
-    form.getFields().forEach(field => {
-    field.enableReadOnly();
-   // form.flatten(); // Torna os campos não editáveis
+   form.flatten(); // Torna os campos não editáveis
 
     const pdfBytes = await pdfDoc.save();
     const nomeCliente = (dados.cliente || "cliente").replace(/\s+/g, "_").toUpperCase();
